24829759883ef2bc3df847371c056fea78bbdaaa
loghogjog <103980325+loghogjog@users.noreply.github.com>
2022-04-19 12:53:33 +0800

    Question 5 half done

--- Question5.java
+++ Question5.java
@@ -27,6 +27,13 @@ public class Question5
      */
      
     Scanner in = new Scanner(System.in);
+    System.out.println("Enter number of loops: ");
+    int loops = in.nextInt();
+    
+    While(loops > 0) {
+      System.out.println("Enter an integer: ");
+      
+    }
     
   }
 }
